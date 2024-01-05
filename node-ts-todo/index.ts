364703e8cad9dc98b849f1b273b4896005937851
Bablu Mia <mdbablu22.bablu22@gmail.com>
2024-01-05 23:34:07 +0600

    Update index.ts

--- node-ts-todo/index.ts
+++ node-ts-todo/index.ts
@@ -25,7 +25,7 @@ app.use(
 // Database connection
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 3306,
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
